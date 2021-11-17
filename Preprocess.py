7d4e936570dd728f2930896772f9fcb98b542635
1uoshan <81515396+Looooook@users.noreply.github.com>
2021-11-17 23:02:17 +0800

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -1,15 +1,14 @@
 import numpy as np
 from PIL import Image
 import os
+import torch
 
 
-class Image_to_Numpy():
-    def __init__(self, file_path, output_path, dog_types, width=100, height=100):
+class Image_to_Numpy(object):
+    def __init__(self, file_path, output_path, dog_types):
         self.file_path = file_path
-        self.output_path = output_path
         self.dog_types = dog_types
-        self.width = width
-        self.height = height
+        self.output_path = output_path
 
     def image_rename(self):
         type_counter = 0
@@ -22,12 +21,12 @@ class Image_to_Numpy():
                 instance_counter += 1
             type_counter += 1
 
-    def image_resize(self):
+    def image_resize(self, width=100, height=100):
         for type in self.dog_types:
             for dog_instance in os.listdir(self.file_path + type):
                 img = Image.open(self.file_path + type + '/' + dog_instance)
                 img_RGB = img.convert('RGB')
-                resized_image = img_RGB.resize((self.width, self.height), Image.BILINEAR)
+                resized_image = img_RGB.resize((width, height), Image.BILINEAR)
                 resized_image.save(os.path.join(self.output_path, dog_instance))
 
     def imgs_to_np(self):
@@ -37,11 +36,13 @@ class Image_to_Numpy():
             img = Image.open(self.output_path + dog_instance)
             img = np.array(img)
             list_images.append(img)
-            list_labels.append(dog_instance.split('_')[0])
+            list_labels.append(int(dog_instance.split('_')[0]))
         # list_images此时还是列表
-        np_images = np.array(list_images)
+        np_images = np.array(list_images).transpose((0, 3, 1, 2))
         np_labels = np.array(list_labels)
-        return np_images, np_labels
+        # print(np_images.shape)
+        # / 255.0
+        return torch.from_numpy(np_images).float() / 255, torch.from_numpy(np_labels).long()
 
 
 if __name__ == '__main__':
@@ -50,4 +51,4 @@ if __name__ == '__main__':
     # image_to_numpy.image_rename()
     # image_to_numpy.image_resize()
     np_image, np_labels = image_to_numpy.imgs_to_np()
-    print(np_image.shape, np_labels.shape)
+    print(np_image.shape, np_labels)
